eae102802b2cd3eaf11646082433790282db4af1
FRATEA STEFANO <stefano.fratea@esternibisp.com>
2026-01-26 15:59:14 +0100

    new url fe

--- src/main/java/com/example/cleaning/configuration/CorsConfig.java
+++ src/main/java/com/example/cleaning/configuration/CorsConfig.java
@@ -8,13 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    public static final String URL_FE = "https://shitty-project.onrender.com";
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")                 // tutte le API
-                        .allowedOrigins("https://cleaning-fe.onrender.com") // frontend Render
+                        .allowedOrigins(URL_FE) // frontend Render
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
